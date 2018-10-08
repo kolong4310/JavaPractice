5aa55f94088c25273c3afc837d19308bb50e1694
kolong4310 <kolong4310@gmail.com>
2018-10-08 17:15:36 +0900

    숫자열 예제
    덧셈,소수점등등 계산해보기

---
+++ java_tutorials/src/org/tutorial/number/Number.java
@@ -0,0 +1,13 @@
+package org.tutorial.number;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1+2);
+		System.out.println(1.2+2.3);
+		System.out.println(2*5);
+		System.out.println(6/3);
+	}
+
+}
