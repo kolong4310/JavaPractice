2305d7ff0842370c96757461766605852d4e99b1
kolong4310 <kolong4310@gmail.com>
2018-10-08 17:23:24 +0900

    변수 연습
    기초적인 변수 선언과 화면 출력 연습

---
+++ java_tutorials/src/org/tutorial/veriable/IntergerDemo.java
@@ -0,0 +1,30 @@
+package org.tutorial.veriable;
+
+public class IntergerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a;
+		a = 1;
+		System.out.println(a+1);
+		
+		a = 2;
+		System.out.println(a+1);
+				
+		double b;
+		b = 1.1;
+		
+		System.out.println(b+1.1);
+		
+		String first,second;
+		first = "coding";
+		second = "everybody";
+		
+		System.out.println(first +" "+ second);
+		
+		
+		
+	}
+
+}
