f05f5add9767fb78e3b5f0c4d2f70f80a8c56fb7
kolong4310 <kolong4310@gmail.com>
2018-10-10 13:02:09 +0900

    배열 사용 기본
    배열 사용의 정의와 기초 개념

---
+++ JavaPratice/java_tutorials/src/org/tutorial/array/DefineDemo.java
@@ -0,0 +1,24 @@
+package org.tutorial.array;
+
+public class DefineDemo {
+	public static void main(String[] args) {
+		
+		String[] classGroup ={"ÃÖÁøÇö", "ÃÖÀ¯ºó", "ÇÑ¾Æ¶÷","±èÃ·Áö"};
+		System.out.println(classGroup[0]);
+		System.out.println(classGroup[2]);
+		System.out.println(classGroup[1]);
+		System.out.println(classGroup[3]);
+		
+		
+		String[] classGroup1 = new String[4];
+		classGroup1[0] = "°¡³ª´Ù";
+		System.out.println(classGroup1.length);
+		classGroup1[1] = "±è¶Òµü";
+		System.out.println(classGroup1.length);
+		classGroup1[2] = "±è¶ÊÀÌ";
+		System.out.println(classGroup1.length);
+		classGroup1[3] = "ÀÌ»ìÇØ";
+		System.out.println(classGroup1.length);
+		
+	}
+}
