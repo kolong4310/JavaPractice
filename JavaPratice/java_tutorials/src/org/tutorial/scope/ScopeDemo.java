d0f828fb5689e51f25ce7d17959fe2101c0cd68b
kolong4310 <kolong4310@gmail.com>
2018-10-11 13:52:12 +0900

    유효범위
    유효범위가 무엇인지 이해 - 메소드 안에서 변수를 선언하면 그것은 메소드 안에서만 유효한 것이 된다.

---
+++ JavaPratice/java_tutorials/src/org/tutorial/scope/ScopeDemo.java
@@ -0,0 +1,18 @@
+package org.tutorial.scope;
+
+
+
+public class ScopeDemo {
+	
+	static void a(){
+		int i = 0;
+	}
+	
+	public static void main(String[] args) {
+		
+		for (int i=0; i<5; i++){
+			a();
+			System.out.println(i);
+		}
+	}
+}
